ae60c06d334be7e05fe3217fd58a4ac61f705179
mleach97 <98606574+mleach97@users.noreply.github.com>
2022-02-21 21:39:34 -0500

    Added comments

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ let cEG = require('./computeExam');
 let cFE = require('./computeFinal');
 
 //here you can test values
-console.log(cHW.computeHW(100, 100, 100));
-console.log(cEG.computeExams(100, 100));
-console.log(cFE.getFinalGrade(20, 80));
+console.log(cHW.computeHW(100, 100, 100));//put in three hw grades here
+console.log(cEG.computeExams(100, 100));//put in two exam grades here
+console.log(cFE.getFinalGrade(20, 80)); 
+//first: Hw final hw grade (0 to 20) 
+//second: Exam final grade (0 to 80)
