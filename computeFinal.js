8aa087b874b0b26de9f62d8728ce762d6d1883b0
mleach97 <98606574+mleach97@users.noreply.github.com>
2022-02-21 21:36:27 -0500

    Added a upper bound for the inputs and fixed the rounding code

--- computeFinal.js
+++ computeFinal.js
@@ -1,15 +1,18 @@
 exports.getFinalGrade = function(hwGrade, examGrade){
-    if(hwGrade == -1 || examGrade == -1){
+    if(hwGrade < 0 || examGrade < 0){//Lower bound
+        return "ERROR! Invalid grade put in homework or exams.";
+    }else if(hwGrade > 20 || examGrade > 80){//Upper bound
         return "ERROR! Invalid grade put in homework or exams.";
     }
 
     //Compute final exam grade by adding the two functions together
     var betaExamGrade = hwGrade + examGrade;
 
-    //Now we need a way to round the function up if it goes above .5
-
+    //The code below checks if the statement can be rounded up
     if(betaExamGrade + 0.5 > Math.floor(betaExamGrade) + 1){
         betaExamGrade = Math.floor(betaExamGrade) + 1;
+    }else{
+        betaExamGrade = Math.floor(betaExamGrade);
     }
 
     return betaExamGrade;
